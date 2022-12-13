575d446683c6c1ced765d7d9aee7b929e93c5be6
ChornyiYevhenii <gibridius16@gmail.com>
2022-12-13 20:37:23 +0200

    fixed

---
+++ src/js/task-07.js
@@ -0,0 +1,12 @@
+const refs = {
+  inputEl: document.querySelector('#font-size-control'),
+  textEl: document.querySelector('#text'),
+};
+
+refs.inputEl.value = '16';
+refs.inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+  refs.textEl.style.fontSize = `${event.currentTarget.value}px`;
+}
+
